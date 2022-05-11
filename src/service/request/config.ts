b0bf45279c878c65e4913be4368319a69638facc
311456 <58513498+311456@users.noreply.github.com>
2022-05-11 10:34:39 +0800

    Update config.ts

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,7 +4,7 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://152.136.185.210:5000'
+  BASE_URL = 'http://152.136.185'
 } else {
   BASE_URL = 'http://xxxx'
 }
