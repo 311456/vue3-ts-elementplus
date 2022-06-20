db0b8f56e832daaf9658b803e6ec779c13e13b10
harr <2826455328@qq.com>
2022-06-20 11:05:46 +0800

    fix: fixed bug

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -4,9 +4,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'http://152.136.185:5000'
+  BASE_URL = 'http://152.136.185.210:5000'
 } else {
-  BASE_URL = 'http://152.136.185:5000'
+  BASE_URL = 'http://152.136.185.210:5000'
 }
 
 export { BASE_URL, TIME_OUT }
